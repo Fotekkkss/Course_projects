cb997915638f8c5ea9f9a521e1dff6319bc80047
Yehor <yhr@gmail.com>
2024-10-03 12:09:16 +0200

    Set city name

---
+++ TestProject/src/test/java/HotelSearch.java
@@ -0,0 +1,29 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class HotelSearch {
+    @Test
+    public void searchHotel(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        ChromeOptions optionsChrome = new ChromeOptions();
+        driver.manage().timeouts().implicitlyWait(10L, TimeUnit.SECONDS);
+        optionsChrome.addArguments("--disable-search-engine-choice-screen");
+        driver.manage().window().maximize();
+        driver.get("http://www.kurs-selenium.pl/demo/");
+
+        driver.findElement(By.className("select2-chosen")).click();
+        driver.findElement(By.xpath("//div[@id='select2-drop']//input")).sendKeys("Dubai");
+        driver.findElement(By.xpath("//span[@class='select2-match' and text()='Dubai']")).click();
+
+
+
+
+    }
+}

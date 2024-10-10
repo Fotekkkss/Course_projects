f1f97bb22127bc99e8bc72f88b408931b8492137
Yehor <yhr@gmail.com>
2024-10-10 11:13:02 +0200

    Added config file

--- TestProject/src/test/java/pl/seleniumdemo/tests/BaseTest.java
+++ TestProject/src/test/java/pl/seleniumdemo/tests/BaseTest.java
@@ -6,13 +6,14 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import pl.seleniumdemo.utils.DriverFactory;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
     protected WebDriver driver;
     @BeforeMethod
-    public void setup(){
-        driver = DriverFactory.getDriver("google");
+    public void setup() throws IOException {
+        driver = DriverFactory.getDriver();
         ChromeOptions optionsChrome = new ChromeOptions();
         driver.manage().timeouts().implicitlyWait(10L, TimeUnit.SECONDS);
         optionsChrome.addArguments("--disable-search-engine-choice-screen");

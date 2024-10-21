6bf76cee50cd49914a226f310eda498156e2c629
Yehor <yhr@gmail.com>
2024-10-21 14:17:39 +0200

    Add additional test: but all listed products

--- seleniumdemo/src/test/java/pl/testeroprogramowania/pages/OrderDetailsPage.java
+++ seleniumdemo/src/test/java/pl/testeroprogramowania/pages/OrderDetailsPage.java
@@ -10,12 +10,33 @@ public class OrderDetailsPage {
     @FindBy(xpath = "//p[@class='woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received']")
     private WebElement orderNotice;
 
+    @FindBy(xpath = "//td[contains(@class, 'product-name')]")
+    private WebElement productName;
+
+    @FindBy(partialLinkText = "Java Selenium WebDriver")
+    private WebElement productNameSELENIUM;
+
+    @FindBy(partialLinkText = "GIT basics")
+    private WebElement productNameGIT;
+
+    @FindBy(partialLinkText = "BDD Cucumber")
+    private WebElement productNameBDD;
+
     public WebElement getProductName() {
         return productName;
     }
 
-    @FindBy(xpath = "//td[contains(@class, 'product-name')]")
-    private WebElement productName;
+    public WebElement getGitName() {
+        return productNameGIT;
+    }
+
+    public WebElement getBddName() {
+        return productNameBDD;
+    }
+
+    public WebElement getSeleniumName() {
+        return productNameSELENIUM;
+    }
 
     public OrderDetailsPage(WebDriver driver) {
         PageFactory.initElements(driver, this);
--- seleniumdemo/src/test/java/pl/testeroprogramowania/tests/CheckoutTest.java
+++ seleniumdemo/src/test/java/pl/testeroprogramowania/tests/CheckoutTest.java
@@ -5,8 +5,11 @@ import org.testng.annotations.Test;
 import pl.testeroprogramowania.models.Customer;
 import pl.testeroprogramowania.pages.HomePage;
 import pl.testeroprogramowania.pages.OrderDetailsPage;
+import pl.testeroprogramowania.pages.ProductPage;
 
 public class CheckoutTest extends BaseTest {
+
+    private String[] titles = {"Java Selenium Webdriver", "BDD cucumber", "GIT basics"};
 
     @Test
     public void checkoutTest() throws InterruptedException {
@@ -39,5 +42,35 @@ public class CheckoutTest extends BaseTest {
 
         Assert.assertEquals(orderDetailsPage.getOrderNotice().getText(), "Thank you. Your order has been received.");
         Assert.assertEquals(orderDetailsPage.getProductName().getText(), "Java Selenium WebDriver × 15");
+    }
+
+    @Test
+    public void checkoutAllroductsTest() throws InterruptedException {
+        Customer customer = new Customer();
+
+
+        ProductPage orderDetailsPageJava = new HomePage(driver)
+                .openShopPage()
+                .openProduct("Java Selenium WebDriver")
+                .addProductToCart();
+
+        ProductPage orderDetailsPageBDD = new HomePage(driver)
+                .openShopPage()
+                .openProduct("BDD Cucumber")
+                .addProductToCart();
+
+        OrderDetailsPage orderDetailsPage = new HomePage(driver)
+                .openShopPage()
+                .openProduct("GIT basics")
+                .addProductToCart()
+                .viewCart()
+                .openAddressDetails()
+                .fillAddressDetails(customer, "Some comment)");
+
+       Assert.assertEquals(orderDetailsPage.getOrderNotice().getText(), "Thank you. Your order has been received.");
+       Assert.assertEquals(orderDetailsPage.getSeleniumName().getText(), "Java Selenium WebDriver");
+       Assert.assertEquals(orderDetailsPage.getGitName().getText(), "GIT basics");
+       Assert.assertEquals(orderDetailsPage.getBddName().getText(), "BDD Cucumber");
+
     }
 }
